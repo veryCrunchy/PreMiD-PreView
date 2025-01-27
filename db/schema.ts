3af3d07dd0be4f7c66b2281041bde223aaf7d57e
veryCrunchy <me@verycrunchy.dev>
2025-01-27 01:34:15 +0100

    feat: database schema

---
+++ db/schema.ts
@@ -0,0 +1,68 @@
+import { relations } from "drizzle-orm";
+import { sqliteTable, text, integer, blob } from "drizzle-orm/sqlite-core";
+
+// Tables
+//TODO: check when well rested, schema likely has some flaws 
+export const activities = sqliteTable("activities", {// the shares created
+    id: integer().primaryKey(),
+    createdAt: text().default("CURRENT_TIMESTAMP"),
+});
+
+export const files = sqliteTable("files", {
+    id: integer().primaryKey(),
+    name: text().notNull(),
+    hash: text().notNull().unique(), // Hash to avoid duplicates
+    data: blob().notNull(),
+    createdAt: text().default("CURRENT_TIMESTAMP").notNull(),
+});
+
+export const revisions = sqliteTable("revisions", {
+    id: integer().primaryKey(),
+    folderId: integer().references(() => activities.id),
+    number: integer().notNull(), // Revision number
+    createdAt: text().default("CURRENT_TIMESTAMP").notNull(),
+});
+
+export const revisionFiles = sqliteTable("revisionFiles", {
+    // Link files to a revision
+    // This helps save storage by reusing unchanged files across different revisions
+    id: integer().primaryKey(),
+    revisionId: integer().references(() => revisions.id),
+    fileId: integer().references(() => files.id),
+    createdAt: text().default("CURRENT_TIMESTAMP").notNull(),
+});
+
+export const metadata = sqliteTable("metadata", {
+    // metadata is stored separate from files to ensure easy access
+    id: integer().primaryKey(),
+    revisionId: integer().references(() => revisions.id),
+    hash: text().notNull().unique(), // Hash to avoid duplicates
+    json: text().notNull(),
+    createdAt: text().default("CURRENT_TIMESTAMP").notNull(),
+});
+
+// Relations
+export const activityRelations = relations(activities, ({ many }) => ({
+    revisions: many(revisions),
+}));
+
+export const revisionsRelations = relations(revisions, ({ many, one }) => ({
+    distFolder: one(activities),
+    revisionFiles: many(revisionFiles),
+    metadata: one(metadata),
+}));
+
+export const revisionFilesRelations = relations(revisionFiles, ({ many, one }) => ({
+    revision: one(revisions),
+    file: one(files),
+}));
+
+// potentially unnecessary relations
+
+// export const filesRelations = relations(files, ({ many }) => ({
+//     revisionFile: many(revisionFiles),
+// }));
+
+// export const metadataRelations = relations(metadata, ({ one }) => ({
+//     revision: one(revisions), // should be many
+// }));
