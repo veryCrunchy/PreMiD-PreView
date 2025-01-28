96a1f96d6722cae1b81d66f75f42f29bcf92f864
veryCrunchy <me@verycrunchy.dev>
2025-01-28 23:58:41 +0100

    chore: css reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  css: ["@unocss/reset/normalize.css", "@unocss/reset/eric-meyer.css"],
   modules: ["@unocss/nuxt", "@vueuse/nuxt"],
 });
