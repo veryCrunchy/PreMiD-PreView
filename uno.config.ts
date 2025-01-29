f3fe27db09445303bc4cce5234f48992d4384673
veryCrunchy <me@verycrunchy.dev>
2025-01-29 21:48:33 +0100

    chore: unocss configs

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,22 @@
-import presetUno from '@unocss/preset-uno'
-import { defineConfig } from 'unocss'
+import presetUno from "@unocss/preset-uno";
+import transformerDirectives from "@unocss/transformer-directives";
+import { defineConfig } from "unocss";
 
 export default defineConfig({
-  presets: [
-    presetUno(),
+  presets: [presetUno()],
+  transformers: [transformerDirectives()],
+  theme: {
+    fontFamily: {
+      DEFAULT: "Inter",
+    },
+  },
+  preflights: [
+    {
+      getCSS: ({ theme }) => `
+        body {
+          font-family: ${theme.fontFamily!.DEFAULT};
+        }
+      `,
+    },
   ],
-})
+});
