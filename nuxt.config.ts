1b66baa41a7e957958ed6c84f52cec02e7fab255
veryCrunchy <me@verycrunchy.dev>
2025-01-29 21:46:54 +0100

    chore: add nuxt fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["@unocss/reset/normalize.css", "@unocss/reset/eric-meyer.css"],
-  modules: ["@unocss/nuxt", "@vueuse/nuxt", "nuxt-auth-utils"],
+  modules: ["@unocss/nuxt", "@vueuse/nuxt", "nuxt-auth-utils", "@nuxt/fonts"],
   devServer: {
     host: "dev.crun.zip",
   },
